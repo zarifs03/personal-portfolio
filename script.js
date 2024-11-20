1257f0b45dcf95c9c667abfd9eecbccc00cd8cbe
Zarif-Solaiman <23640056@student.uwa.edu.au>
2024-11-20 17:43:26 +0800

    added custom cursor and associated js

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function scrollToProjects() {
         projectsSection.scrollIntoView({ behavior: 'smooth' });
     }
 }
+
+const cursor = document.querySelector('.cursor');
+
+    document.addEventListener('mousemove', (e) => {
+      cursor.style.left = e.clientX + 'px';
+      cursor.style.top = e.clientY + 'px';
+});
